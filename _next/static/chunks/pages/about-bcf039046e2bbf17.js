64e7f66fcf74235da9cd0632f663cb0421b389a5
edgarkwon <edgarkwon@musicow.com>
2022-08-04 14:59:46 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/chunks/pages/about-bcf039046e2bbf17.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{512:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return c(414)}])},414:function(a,b,c){"use strict";function d(){return"About"}c.r(b),c.d(b,{default:function(){return d}})}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=512)}),_N_E=a.O()}])
---
+++ _next/static/chunks/pages/songs-fd92781967a09e82.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[750],{8226:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/songs",function(){return c(6098)}])},6098:function(a,b,c){"use strict";function d(){return"Songs"}c.r(b),c.d(b,{default:function(){return d}})}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=8226)}),_N_E=a.O()}])
